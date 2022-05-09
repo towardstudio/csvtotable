953596149a809c0c5dd7ddd9b1f537e8371dcd8c
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2022-05-09 12:34:12 +0100

    Update for Prettier

--- src/CSVToTable.php
+++ src/CSVToTable.php
@@ -30,9 +30,9 @@ class CSVToTable extends Plugin
 {
     public static ?CSVToTable $plugin;
 
-	public bool $hasCpSection = true;
+    public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
-	public static ?Settings $settings;
+    public static ?Settings $settings;
 
     // Public Methods
     // =========================================================================
@@ -41,44 +41,35 @@ class CSVToTable extends Plugin
     {
         parent::init();
         self::$plugin = $this;
-		self::$settings = $this->getSettings();
+        self::$settings = $this->getSettings();
 
-		Craft::setAlias('@bar', __DIR__);
+        Craft::setAlias("@bar", __DIR__);
 
-		$this->_registerControlPanel();
-		$this->_registerFieldTypes();
-		$this->_registerExtensions();
+        $this->_registerControlPanel();
+        $this->_registerFieldTypes();
+        $this->_registerExtensions();
 
-		// Handler: UrlManager::EVENT_REGISTER_CP_URL_RULES
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                Craft::debug(
-                    "UrlManager::EVENT_REGISTER_CP_URL_RULES",
-                    __METHOD__
-                );
-                // Register our Control Panel routes
-                $event->rules = array_merge(
-                    $event->rules,
-                    $this->customAdminCpRoutes()
-                );
+        // Handler: UrlManager::EVENT_REGISTER_CP_URL_RULES
+        Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function (
+            RegisterUrlRulesEvent $event
+        ) {
+            Craft::debug("UrlManager::EVENT_REGISTER_CP_URL_RULES", __METHOD__);
+            // Register our Control Panel routes
+            $event->rules = array_merge($event->rules, $this->customAdminCpRoutes());
+        });
+
+        Event::on(Cp::class, Cp::EVENT_REGISTER_CP_NAV_ITEMS, function (
+            RegisterCpNavItemsEvent $event
+        ) {
+            if (Craft::$app->config->general->devMode) {
+                return;
             }
-        );
-
-		Event::on(
-            Cp::class,
-            Cp::EVENT_REGISTER_CP_NAV_ITEMS,
-            function(RegisterCpNavItemsEvent $event) {
-                if (Craft::$app->config->general->devMode) return;
-                $csvPlugin = array_search(
-                    'csvtotable',
-                    array_column($event->navItems, 'url')
-                );
-                if ($csvPlugin === false) return;
-                unset($event->navItems[$csvPlugin]);
+            $csvPlugin = array_search("csvtotable", array_column($event->navItems, "url"));
+            if ($csvPlugin === false) {
+                return;
             }
-        );
+            unset($event->navItems[$csvPlugin]);
+        });
 
         Craft::info(
             Craft::t("csvtotable", "{name} plugin loaded", [
@@ -88,17 +79,17 @@ class CSVToTable extends Plugin
         );
     }
 
-	/**
-	 * Registers the field type provided by this plugin.
-	 * @param RegisterComponentTypesEvent $event The event.
-	 * @return void
-	 */
-	public function registerFieldTypes(RegisterComponentTypesEvent $event)
-	{
-		$event->types[] = CSVToTableField::class;
-	}
+    /**
+     * Registers the field type provided by this plugin.
+     * @param RegisterComponentTypesEvent $event The event.
+     * @return void
+     */
+    public function registerFieldTypes(RegisterComponentTypesEvent $event)
+    {
+        $event->types[] = CSVToTableField::class;
+    }
 
-	// Rename the Control Panel Item & Add Sub Menu
+    // Rename the Control Panel Item & Add Sub Menu
     public function getCpNavItem(): ?array
     {
         // Set additional information on the nav item
@@ -121,8 +112,7 @@ class CSVToTable extends Plugin
         return $item;
     }
 
-
-	// Protected Methods
+    // Protected Methods
     // =========================================================================
 
     /**
@@ -133,13 +123,13 @@ class CSVToTable extends Plugin
         return new Settings();
     }
 
-	public function getSettingsResponse(): mixed
+    public function getSettingsResponse(): mixed
     {
         // Just redirect to the plugin settings page
-        return Craft::$app->getResponse()->redirect(UrlHelper::cpUrl('csvtotable/settings'));
+        return Craft::$app->getResponse()->redirect(UrlHelper::cpUrl("csvtotable/settings"));
     }
 
-	/**
+    /**
      * Return the custom Control Panel routes
      *
      * @return array
@@ -147,49 +137,32 @@ class CSVToTable extends Plugin
     protected function customAdminCpRoutes(): array
     {
         return [
-			"csvtotable/settings" =>
-                "csvtotable/settings/plugin-settings",
-            "csvtotable" =>
-                "csvtotable/settings/plugin-settings",
+            "csvtotable/settings" => "csvtotable/settings/plugin-settings",
+            "csvtotable" => "csvtotable/settings/plugin-settings",
         ];
     }
 
-	// Private Methods
+    // Private Methods
     // =========================================================================
 
-	private function _registerControlPanel()
-	{
-		Event::on(
-            View::class,
-            View::EVENT_REGISTER_CP_TEMPLATE_ROOTS,
-            function (RegisterTemplateRootsEvent $e) {
-                if (
-                    is_dir(
-                        $baseDir =
-                            $this->getBasePath() .
-                            DIRECTORY_SEPARATOR .
-                            "templates"
-                    )
-                ) {
-                    $e->roots[$this->id] = $baseDir;
-                }
+    private function _registerControlPanel()
+    {
+        Event::on(View::class, View::EVENT_REGISTER_CP_TEMPLATE_ROOTS, function (
+            RegisterTemplateRootsEvent $e
+        ) {
+            if (is_dir($baseDir = $this->getBasePath() . DIRECTORY_SEPARATOR . "templates")) {
+                $e->roots[$this->id] = $baseDir;
             }
-        );
-	}
+        });
+    }
 
-	private function _registerFieldTypes()
-	{
-		Event::on(
-			Fields::class,
-			Fields::EVENT_REGISTER_FIELD_TYPES, [
-				$this, 'registerFieldTypes'
-			]
-		);
-	}
+    private function _registerFieldTypes()
+    {
+        Event::on(Fields::class, Fields::EVENT_REGISTER_FIELD_TYPES, [$this, "registerFieldTypes"]);
+    }
 
-	private function _registerExtensions()
-	{
-		Craft::$app->view->registerTwigExtension(new CsvToTableTwigExtension());
-	}
-
+    private function _registerExtensions()
+    {
+        Craft::$app->view->registerTwigExtension(new CsvToTableTwigExtension());
+    }
 }
